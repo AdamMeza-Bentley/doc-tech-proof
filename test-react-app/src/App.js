a7d6252b7ab9a97c8c644190b1e5e83453aafc68
Adam Meza - Intern <adammeza-bentley@users.noreply.github.com>
2023-11-02 14:44:05 -0400

    update node moodules

--- test-react-app/src/App.js
+++ test-react-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react';
-import treeData from './tree.json';
+import main from './main.json';
+import bis from './bis.json'
+import presentation from './presentation.json'
 import { LeftNav } from './Components/LeftNav/LeftNav';
-import { Link, NavLink } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 // possible URLS are prefrence, presentation and learning
 // also noticed that front end and backend arent formnatted the same. they have no URL change and no LeftNav Cahange
@@ -12,10 +14,20 @@ import { Link, NavLink } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-        <LeftNav data={treeData}/>
-        <div className='main'>
-          <button>Switch Content</button>        
-        </div>
+      <header className='header'>
+        <a className ='link' href="/presentation"> PRESENTATION</a>
+        <a className ='link' href="/"> main </a>
+        <a className ='link' href="/bis"> BIS</a>
+      </header>
+      <main>
+        <Switch>
+          <Route path='/' render={() => <LeftNav data={main}/>}/>
+          <Route path='/presentation' render={() => <LeftNav data={presentation}/>}/>
+          <Route path='/bis' render={() => <LeftNav data={bis}/>}/>
+        </Switch>
+
+        <div className='main'></div>
+      </main>
     </div>
   );
 };
